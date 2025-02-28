eee83c5f6776dae980800ee29f3f9d9e9a0ca54f
uuTrunk <uu_Crush@outlook.com>
2025-02-28 17:57:26 +0800

    Delete cart directory

--- cart/com.example.cart.CartApplicationTests.java
+++
@@ -1,8 +0,0 @@
-
-@Test
-void testAddItem() throws Exception {
-    mockMvc.perform(post("/api/cart/123/items")
-                    .param("productId", "456")
-                    .param("quantity", "2"))
-            .andExpect(status().isCreated()); // 确保预期状态码是201
-}
--- cart/src/main/java/com/example/cart/model/Product.java
+++
@@ -1,4 +0,0 @@
-package com.example.cart.model;
-
-public class Product {
-}
