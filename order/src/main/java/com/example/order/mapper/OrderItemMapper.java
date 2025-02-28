255b74485bded3200fc909edc69a3277add0c0ca
uuTrunk <uu_Crush@outlook.com>
2025-02-28 17:57:15 +0800

    Delete order directory

--- order/src/main/java/com/example/order/mapper/OrderItemMapper.java
+++
@@ -1,8 +0,0 @@
-package com.example.order.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.order.entity.OrderItem;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface OrderItemMapper extends BaseMapper<OrderItem> {}
--- order/src/main/java/com/example/order/service/OrderService.java
+++
@@ -1,8 +0,0 @@
-package com.example.order.service;
-
-import com.example.order.vo.OrderCreateVO;
-
-public interface OrderService {
-    String createOrder(OrderCreateVO vo);
-    boolean updateOrderStatus(String orderId, String status);
-}
